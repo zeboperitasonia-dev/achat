a12be295bd028969347759bb416cb39b7a461fac
zeboperitasonia-dev <zeboperitasonia@gmail.com>
2026-03-14 02:43:39 +0100

    contenu modifier

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BoutonLite } from "../bouton-lite/bouton-lite";
-import { CommonModule } from '@angular/common';
-import { Accueil } from "../accueil/accueil";
 import { BoutonPanier } from "../bouton-panier/bouton-panier";
 import { AppComponent } from "../app-component/app-component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BoutonLite, CommonModule, Accueil, BoutonPanier, AppComponent],
+  imports: [RouterOutlet, BoutonPanier, AppComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
